5d4a64982210e23090a360c8869abe0c0c684736
Norwill <willeverett206@gmail.com>
2024-11-10 12:42:20 -0500

    First commit
    First commit everything that will did is done

---
+++ Fighter Planes F24/Assets/Scripts/Coin.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            GameObject.Find("GameManager").GetComponent<GameManager>().EarnScore(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
